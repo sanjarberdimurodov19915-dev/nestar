e88bf115e41a41e492661a50cc127b87c88171f2
Sanjar (SAM) <sanjarberdimurodov19915@gmail.com>
2026-08-25 20:54:33 +0900

    fix: modify view module to record visits

---
+++ apps/nestar-api/src/libs/dto/view/view.input.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from "@nestjs/graphql";
+import { IsNotEmpty } from "class-validator";
+import { ViewGroup } from "../../enums/view.enum";
+import type { ObjectId } from "mongoose";
+
+
+@InputType()
+export class ViewInput {
+
+    @IsNotEmpty()
+    @Field(() => String)
+    memberId: ObjectId;
+
+    @IsNotEmpty()
+    @Field(() => String)
+    viewRefId: ObjectId;
+    
+    @IsNotEmpty()
+    @Field(() => ViewGroup)
+    viewGroup: ViewGroup;
+
+}
