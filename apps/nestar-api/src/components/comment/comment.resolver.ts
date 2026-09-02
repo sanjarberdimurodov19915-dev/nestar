ce00918df19d578c7cce427606635abeb2d47813
Sanjar (SAM) <sanjarberdimurodov19915@gmail.com>
2026-09-02 18:34:54 +0900

    feat: develop removeCommentByAdmin GraphQL api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -9,6 +9,9 @@ import type { ObjectId } from 'mongoose';
 import { Comments, Comment } from '../../libs/dto/comment/comment';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { WithoutGuard } from '../auth/guards/without.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 
 @Resolver()
@@ -45,5 +48,16 @@ export class CommentResolver {
         console.log('Query: getComments');
         input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
         return await this.commentService.getComments(memberId, input);
+    }
+
+    /**Admin **/
+
+    @Roles(MemberType.ADMIN)
+    @UseGuards(RolesGuard)
+    @Mutation((returns) => Comment)
+    public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+        console.log('Mutation: removeCommentByAdmin');
+        const commentId = shapeIntoMongoObjectId(input);
+        return await this.commentService.removeCommentByAdmin(commentId);
     }
 }
