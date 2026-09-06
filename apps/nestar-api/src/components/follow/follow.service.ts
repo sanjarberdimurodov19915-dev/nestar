4b4640349fdb611b19a868735a115d54a32e6897
Sanjar (SAM) <sanjarberdimurodov19915@gmail.com>
2026-09-06 19:46:47 +0900

    feat: integration of lookupAuthMemberFollowed complex query business logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 
 @Injectable()
@@ -81,6 +81,7 @@ export class FollowService {
                             // meLiked
                             lookupAuthMemberLiked(memberId, '$followingId'),
                             // meFollowed
+                            lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId' }),
                             lookupFollowingData,
                             { $unwind: '$followingData' },
                         ],
@@ -120,6 +121,7 @@ export class FollowService {
                             // meLiked
                             lookupAuthMemberLiked(memberId, '$followerId'),
                             // meFollowed
+                            lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId' }),
                             lookupFollowerData,
                             { $unwind: '$followerData' },
                         ],
